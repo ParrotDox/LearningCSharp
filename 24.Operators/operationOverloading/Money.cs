5f2b44f402a1bc18dc4364b02d5b54e0ccdfbb60
Bibub <slappierR@gmail.com>
2024-10-04 12:33:41 +0500

    operators 24. added

---
+++ 24.Operators/operationOverloading/Money.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    class Coin 
+    {
+        public string name;
+        public Coin() => name = "";
+        public Coin(string n) => name = n;
+    }
+    internal class Cash
+    {
+        private int cash { get; set; }
+        private Coin[] coinArr = new Coin[] { };
+        public Cash() 
+        {
+            cash = 0;
+        }
+        public Cash(int c)
+        {
+            cash = c;
+        }
+        public Cash(int c, Coin[] ca) 
+        {
+            cash = c;
+            coinArr = ca;
+        }
+        public Coin this[int index] 
+        {
+            get 
+            {
+                return coinArr[index];
+            }
+            set 
+            {
+                coinArr[index] = value;
+            }
+        }
+
+        public static Cash operator ++ (Cash m)
+        {
+            Cash temp = new Cash(m.cash);
+            temp.cash += 10;
+            return temp;
+        }
+        public static bool operator == (Cash m1, Cash m2) 
+        {
+            if (m1.cash == m2.cash) return true;
+            else return false;
+        }
+        public static bool operator != (Cash m1, Cash m2)
+        {
+            if (m1.cash != m2.cash) return true;
+            else return false;
+        }
+        //Неявное преобразование implicit явное explicit
+        public static implicit operator double(Cash m) 
+        {
+            return m.cash;
+        }
+        public static explicit operator int(Cash m) 
+        {
+            return m.cash;
+        }
+        public void Print() 
+        {
+            Console.WriteLine($"Cash is {cash}");
+            Console.WriteLine($"Coins are");
+            for(int i = 0; i < coinArr.Length; ++i) 
+            {
+                Console.WriteLine($"{i} Coin: {coinArr[i].name}");
+            }
+        }
+    }
+}
---
+++ 24.Operators/operationOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Money;
+
+namespace operationOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cash m1 = new Cash(15);
+            ++m1;
+            m1++;
+            m1.Print();
+            Cash m2 = new Cash(25000, new Coin[] {new Coin("Trenny"), new Coin("GoldenPhillip") });
+            m2.Print();
+        }
+    }
+}
